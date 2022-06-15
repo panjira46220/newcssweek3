5093d935d56bc5a3e9d93f3828740ba65405069f
panjira <6035512072@psu.ac.th>
2022-06-15 14:57:46 +0700

    cd3

--- components/TradeForm.tsx
+++ components/TradeForm.tsx
@@ -69,7 +69,7 @@ interface TypeOrder{
     }
   }
   let formatdate = dayjs().format("DD/MM/YYYY h:mm");
-    const detail  ={
+    const detail : TypeOrder ={
         time : formatdate,
         symbol: token1+"_"+token2,
         type :"Buy",
